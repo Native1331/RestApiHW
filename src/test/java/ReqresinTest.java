2124f2bf8bac81f588924d052a518b1179d78ec2
Natalya.Kadysheva <Native1331>
2022-06-07 13:46:46 +0300

    Add API Tests

---
+++ src/test/java/ReqresinTest.java
@@ -0,0 +1,86 @@
+import org.junit.jupiter.api.Test;
+
+
+import static org.hamcrest.Matchers.is;
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+
+public class ReqresinTest {
+   @Test
+    void listUsersTest() {
+
+        given()
+                .when()
+                .get("https://reqres.in/api/users?page=2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("total", is(12));
+    }
+
+    @Test
+    void listSingleUserTest() {
+
+        given()
+
+                .when()
+                .get("https://reqres.in/api/users/2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("data.first_name", is("Janet"));
+    }
+
+    @Test
+    void loginTest() {
+        String body = "{ \"name\": \"morpheus\", " +
+                "\"job\": \"leader\" }";
+
+
+
+        given()
+                .log().uri()
+                .log().body()
+                .body(body)
+                .contentType(JSON)
+                .when()
+                .post("https://reqres.in/api/users")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(201)
+                .body("name", is("morpheus"));
+    }
+    @Test
+    void loginUnsuccesfullTest() {
+        String body = "{ \"email\": \"peter@klaven\" }";
+
+
+
+        given()
+                .log().uri()
+                .log().body()
+                .body(body)
+                .contentType(JSON)
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(400)
+                .body("error", is("Missing password"));
+    }
+    @Test
+    void deleteUserTest() {
+                given()
+                .when()
+                .delete("https://reqres.in/api/login")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(204);
+    }
+}
+
